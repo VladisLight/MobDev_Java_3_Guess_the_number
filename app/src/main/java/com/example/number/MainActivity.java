e47338d7601c428e40a24fe0ad9028d10fcbe8fb
VladisLight <VladisLight@gmail.com>
2019-06-09 19:24:08 +0300

    Угадай_число_Бондаренко
    Сделал задание угадай число

--- app/src/main/java/com/example/number/MainActivity.java
+++ app/src/main/java/com/example/number/MainActivity.java
@@ -7,11 +7,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView tvInfo;
     EditText etInput;
     Button bControl;
+    int pickedNumber;
+    boolean gameOver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +26,47 @@ public class MainActivity extends AppCompatActivity {
         etInput = findViewById(R.id.editText);
         bControl = findViewById(R.id.button);
 
+        setupGame();
+    }
+
+    private void setupGame() {
+        gameOver = false;
+        pickedNumber = ThreadLocalRandom.current().nextInt(1, 100 + 1);
+        tvInfo.setText(getResources().getString(R.string.try_to_guess));
+    }
+
+    public void playMore(View view) {
+        setupGame();
     }
 
     public void onClick(View view) {
-        int value = Integer.parseInt(etInput.getText().toString());
-        tvInfo.setText(getResources().getString(R.string.ahead));
+        if (gameOver) {
+            tvInfo.setText("Игра окончена. Сыграть еще?");
+            return;
+        }
+
+        String inputValue = etInput.getText().toString();
+        if (inputValue.isEmpty()) {
+            tvInfo.setText(getResources().getString(R.string.error));
+            return;
+        }
+
+        int value = Integer.parseInt(inputValue);
+
+        if (value < 1 || value > 100) {
+            tvInfo.setText(getResources().getString(R.string.out_of_range));
+            return;
+        }
+
+        if (value > pickedNumber) {
+            tvInfo.setText(getResources().getString(R.string.ahead));
+        } else if (value < pickedNumber) {
+            tvInfo.setText(getResources().getString(R.string.behind));
+        } else {
+            tvInfo.setText(getResources().getString(R.string.hit));
+            gameOver = true;
+        }
+
     }
 }
 
